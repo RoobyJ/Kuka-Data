87d982de4aeafd3c00b558287f4f624065323ec0
RoobyJ <rjureczko19@gmail.com>
2022-09-20 13:50:32 +0200

    rewriten code for visability

--- DataMiner.py
+++ DataMiner.py
@@ -7,110 +7,125 @@ import os
 
 class DataMiner:
     def __init__(self, backup_path, input_string):
-        self.timer1 = 0
-        self.timer2 = 0
-        self.timer3 = 0
+        self.start_time = 0
+        self.full_cycle_time = 0
+        self.excel_creating_time = 0
         self.backup_path = backup_path
         self.input_string = input_string
+        self.wb = Workbook()
 
-    translation_table = dict.fromkeys(map(ord, 'ABCJMXYZ'), None)
+    letters_to_delete = 'ABCJMXYZ'
+    translation_table = dict.fromkeys(map(ord, letters_to_delete), None)
     titles = {'bases': ["base_data", "base_name", "X", "Y", "Z", "A", "B", "C"],
               'tools': ["tool_data", "tool_name", "X", "Y", "Z", "A", "B", "C"],
               'loads': ["load_data", "load_name", "M", "X", "Y", "Z", "A", "B", "C", "Ix", "Iy", "Iz"],
               }
 
     async def create_file(self):
-        self.timer1 = time.time()
-
-        data = {'bases':
-                    {'names': [],
-                     'data': []},
-
-                'tools':
-                    {'names': [],
-                     'data': []},
-
-                'loads':
-                    {'names': [],
-                     'data': []},
-
-                }
+        self.start_time = time.time()
+        # Creating full path of the $config.dat
         config_path = os.path.join(self.input_string, self.backup_path) + "\\KRC\\R1\\System\\$config.dat"
+        # getting data for next steps
+        data = self.organize_data(config_path)
 
-        # reading data from $config.dat file and clearing it to use it later in Excel
+        self.excel_creating_time = time.time() # starting measuring time for each excel
+        self.wb.remove(self.wb['Sheet']) # deleting the default sheet
+        if data is not None:
+            for name in list(data.keys()):
+                self.wb.create_sheet(name)
+                self.configure_excel(name)
+                self.pass_data(data, name)
+
+            # the Excel will be created in the folder where are backups
+            self.wb.save(filename=f"{self.input_string}\\config_data_{self.backup_path}.xlsx")
+            self.full_cycle_time = time.time() # Stop measuring time of the cycle
+
+    # func for performance measure, returns time for one full process
+    def absolute_time(self):
+        if self.start_time != 0 or self.full_cycle_time != 0:
+            return self.full_cycle_time - self.start_time
+
+    # func for testing Excel creating time
+    def excel_time(self):
+        return self.full_cycle_time - self.excel_creating_time
+
+    # reading data from $config.dat file and clearing it to use it later in Excel
+    def organize_data(self, config_path):
+        data_tree = {'bases':
+                         {'names': [],
+                          'data': []},
+
+                     'tools':
+                         {'names': [],
+                          'data': []},
+
+                     'loads':
+                         {'names': [],
+                          'data': []},
+                     }
         if os.path.exists(config_path):
             with open(config_path, "r") as f:
                 for line in f.readlines():
                     if line.startswith("BASE_DATA"):
-                        data['bases']['data'].append(line[line.find("{"):].replace("{", "").replace("}", "")
-                                                     .translate(self.translation_table).split(","))
+                        data_tree['bases']['data'].append(self.clear_data(line))
                     elif line.startswith("TOOL_DATA"):
-                        data['tools']['data'].append(line[line.find("{"):].replace("{", "").replace("}", "")
-                                                     .translate(self.translation_table).split(","))
+                        data_tree['tools']['data'].append(self.clear_data(line))
                     elif line.startswith("LOAD_DATA"):
-                        data['loads']['data'].append(line[line.find("{"):].replace("{", "").replace("}", "")
-                                                     .translate(self.translation_table).split(","))
+                        data_tree['loads']['data'].append(self.clear_data(line))
                     elif line.startswith("BASE_NAME"):
-                        data['bases']['names'].append(line[line.find('"') + 1:-2])
+                        data_tree['bases']['names'].append(self.clear_name(line))
                     elif line.startswith("TOOL_NAME"):
-                        data['tools']['names'].append(line[line.find('"') + 1:-2])
+                        data_tree['tools']['names'].append(self.clear_name(line))
                     elif line.startswith("LOAD_NAME"):
-                        data['loads']['names'].append(line[line.find('"') + 1:-2])
+                        data_tree['loads']['names'].append(self.clear_name(line))
+            return data_tree
 
-            self.timer3 = time.time()
-            wb = Workbook()
-            wb.create_sheet("tools")
-            wb.create_sheet("loads")
-            sheet = wb.active
-            data_key_list = list(data.keys())
-            sheet_num = 0
-
-            for name in data_key_list:
-
-                # Here is the Excel file configured
-                for num1 in range(0, len(self.titles[f'{name}'])):
-                    sheet.title = f"{name}"
-                    ct = sheet[f"{string.ascii_uppercase[num1]}{1}"]
-                    ct.value = self.titles[f'{name}'][num1]
-                    if num1 == 0:
-                        sheet.column_dimensions[get_column_letter(num1 + 1)].width = 10
-                    else:
-                        sheet.column_dimensions[get_column_letter(num1 + 1)].width = 20
-
-                for idx in range(0, 128):
-                    for num in range(0, len(data[f'{name}']['data'][idx])):
-                        if num == 0:
-                            cd = sheet[f"{string.ascii_uppercase[num]}{idx + 2}"]
-                            cd.value = idx + 1
-
-                            cd = sheet[f"{string.ascii_uppercase[num + 2]}{idx + 2}"]
-                            cd.value = float(data[f'{name}']['data'][idx][num])
-                        elif num == 1:
-                            cd = sheet[f"{string.ascii_uppercase[num]}{idx + 2}"]
-                            cd.value = data[f'{name}']['names'][idx]
-                            cd = sheet[f"{string.ascii_uppercase[num + 2]}{idx + 2}"]
-                            cd.value = float(data[f'{name}']['data'][idx][num])
-                        else:
-                            cd = sheet[f"{string.ascii_uppercase[num + 2]}{idx + 2}"]
-                            cd.value = float(data[f'{name}']['data'][idx][num])
-                if len(data.keys()) - 1 == sheet_num:
-                    continue
-                else:
-                    sheet = wb[f"{data_key_list[sheet_num + 1]}"]
-                    sheet_num += 1
-
-            # the Excel will be created in the folder where are backups
-            wb.save(filename=f"{self.input_string}\\config_data_{self.backup_path}.xlsx")
-            self.timer2 = time.time()
         else:
-            self.timer1 = 0
+            self.start_time = 0
             # print(config_path + 'this isn\'t a config!')
 
-    # func for performance measure, returns time for one full process
-    def absolute_time(self):
-        if self.timer1 != 0 or self.timer2 != 0:
-            return self.timer2 - self.timer1
+    # This method returns a list of cleared values
+    def clear_data(self, raw_data):
+        return raw_data[raw_data.find("{"):].replace("{", "").replace("}", "")\
+            .translate(self.translation_table).split(",")
 
-    # func for testing Excel creating time
-    def excel_time(self):
-        return self.timer2 - self.timer3
+    # This method returns a name
+    @staticmethod
+    def clear_name(raw_name):
+        return raw_name[raw_name.find('"') + 1:-2]
+
+    # Here is the Excel file configured, we are changing the name to which we need and resizing the column width
+    def configure_excel(self, name):
+
+        for idx in range(0, len(self.titles[f'{name}'])):
+            sheet = self.wb[f'{name}'] # working sheet
+            record = sheet[f"{string.ascii_uppercase[idx]}{1}"]
+            record.value = self.titles[f'{name}'][idx]
+            if idx == 0:
+                sheet.column_dimensions[get_column_letter(idx + 1)].width = 10
+            else:
+                sheet.column_dimensions[get_column_letter(idx + 1)].width = 20
+
+    # Each sheet will have 129 rows ( 128 will have data )
+    def pass_data(self, data, name):
+        sheet = self.wb[f'{name}'] # working sheet
+        for idx in range(0, 128):
+            for num in range(0, len(data[f'{name}']['data'][idx])):
+                if num == 0:
+                    # here we are passing number from index ( begins from 1 )
+                    record = sheet[f"{string.ascii_uppercase[num]}{idx + 2}"]
+                    record.value = idx + 1
+                    # here is passed the first value with data
+                    record = sheet[f"{string.ascii_uppercase[num + 2]}{idx + 2}"]
+                    record.value = float(data[f'{name}']['data'][idx][num])
+                elif num == 1:
+                    # in this case we pass the name first
+                    record = sheet[f"{string.ascii_uppercase[num]}{idx + 2}"]
+                    record.value = data[f'{name}']['names'][idx]
+                    # and here the second data value
+                    record = sheet[f"{string.ascii_uppercase[num + 2]}{idx + 2}"]
+                    record.value = float(data[f'{name}']['data'][idx][num])
+                else:
+                    # and here are the another values passed into their records
+                    record = sheet[f"{string.ascii_uppercase[num + 2]}{idx + 2}"]
+                    record.value = float(data[f'{name}']['data'][idx][num])
--- main.py
+++ main.py
@@ -4,32 +4,31 @@ import asyncio
 import sys
 from DataMiner import DataMiner
 
-time_list = []
-
 
 # this function is for performance measuring
-def mean():
+def mean(_time_list):
     temp = 0
-    for var in time_list:
+    for var in _time_list:
         temp += var
     return temp
 
 
-async def config_to_excel(_backup, _input_string):
+async def config_to_excel(_backup, _input_string, _time_list):
     obj = DataMiner(_backup, _input_string)
     await obj.create_file()
     resp = obj.absolute_time()
     if resp is not None:
-        time_list.append(resp)
+        _time_list.append(resp)
 
 
 # by making the program async we save much time example for 32 backups async does it in ~2.9s, synchronous needs ~5s
 async def main():
+    time_list = []
     input_string = sys.argv[1]
     if os.path.exists(os.path.dirname(input_string)):
         for backup in os.listdir(input_string):
-            await config_to_excel(backup, input_string)
-        print(mean())
+            await config_to_excel(backup, input_string, time_list)
+        print(mean(time_list))
     else:
         print('given path is incorrect \n should look like this example "C:\\Projects\\NameofProject\\Backups"')
 
